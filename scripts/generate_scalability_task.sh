2d35172709ccd1b0fab97c979fa74d0b61ae7745
Lazy Donkey <lazy@donkey>
2025-10-11 20:31:46 +0700

    change scripts

--- scripts/generate_scalability_task.sh
+++ scripts/generate_scalability_task.sh
@@ -3,6 +3,12 @@
 set -eux
 
 simulator=$HOME/Lazy-Promotions/simulator-concurrent/_build/bin
+output_dir=~/results/
+
+mkdir -p $output_dir
+
+num_iteration=5
+threads_list=(16)
 
 run_simulation()
 {
@@ -12,19 +18,14 @@ run_simulation()
     local cache_size=$4
     local num_threads=$5
 
-    if [[ $algorithm == "fifo" || $algorithm == "lru" ]]; then
-        $simulator/cachesim $algorithm $cache_size --num-thread $num_threads
-    else
-        $simulator/cachesim $algorithm -e $param_name=$param_val $cache_size --num-thread $num_threads
-    fi
+    for iteration in $(seq 1 $num_iteration); do
+        if [[ $algorithm == "fifo" || $algorithm == "lru" ]]; then
+            $simulator/cachesim $algorithm $cache_size --num-thread $num_threads
+        else
+            $simulator/cachesim $algorithm -e $param_name=$param_val $cache_size --num-thread $num_threads
+        fi
+    done
 }
-
-num_iteration=10
-output_dir=~/results/
-mkdir -p $output_dir
-
-threads_list=(16)
-
 
 algo="fifo"
 mkdir -p $output_dir/$algo
@@ -32,9 +33,7 @@ cache_sizes=("94000" "74000" "50000" "24000")
 cache_sizes=("94000")
 for size in "${cache_sizes[@]}"; do
     for threads in "${threads_list[@]}"; do
-        for iteration in $(seq 1 $num_iteration); do
-            run_simulation $algo "" "" $size $threads >> $output_dir/$algo/result_${iteration}.txt
-        done
+        run_simulation $algo "" "" $size $threads >> $output_dir/$algo.txt
     done
 done
 
@@ -44,9 +43,7 @@ cache_sizes=("91000" "65000" "42000" "18000")
 cache_sizes=("91000")
 for size in "${cache_sizes[@]}"; do
     for threads in "${threads_list[@]}"; do
-        for iteration in $(seq 1 $num_iteration); do
-            run_simulation $algo "" "" $size $threads >> $output_dir/$algo/result_${iteration}.txt
-        done
+        run_simulation $algo "" "" $size $threads >> $output_dir/$algo.txt
     done
 done
 
@@ -57,9 +54,7 @@ cache_sizes=("88000")
 for p in 0.9 0.8 0.6 0.5 0.4 0.2 0.1 0.05 0.01; do
     for size in "${cache_sizes[@]}"; do
         for threads in "${threads_list[@]}"; do
-            for iteration in $(seq 1 $num_iteration); do
-                run_simulation $algo "prob" $p $size $threads >> $output_dir/$algo/result_${iteration}.txt
-            done
+            run_simulation $algo "prob" $p $size $threads >> $output_dir/$algo.txt
         done
     done
 done
@@ -71,9 +66,7 @@ cache_sizes=("90000")
 for p in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 ; do
     for size in "${cache_sizes[@]}"; do
         for threads in "${threads_list[@]}"; do
-            for iteration in $(seq 1 $num_iteration); do
-                run_simulation $algo "delay-time" $p $size $threads >> $output_dir/$algo/result_${iteration}.txt
-            done
+            run_simulation $algo "delay-time" $p $size $threads >> $output_dir/$algo.txt
         done
     done
 done
@@ -85,9 +78,7 @@ cache_sizes=("90000")
 for p in 1 2 3 4 5 10 15 20 ; do
     for size in "${cache_sizes[@]}"; do
         for threads in "${threads_list[@]}"; do
-            for iteration in $(seq 1 $num_iteration); do
-                run_simulation $algo "n-bit-counter" $p $size $threads >> $output_dir/$algo/result_${iteration}.txt
-            done
+            run_simulation $algo "n-bit-counter" $p $size $threads >> $output_dir/$algo.txt
         done
     done
 done
@@ -99,9 +90,7 @@ cache_sizes=("90000")
 for p in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 ; do
     for size in "${cache_sizes[@]}"; do
         for threads in "${threads_list[@]}"; do
-            for iteration in $(seq 1 $num_iteration); do
-                run_simulation $algo "batch-size" $p $size $threads >> $output_dir/$algo/result_${iteration}.txt
-            done
+            run_simulation $algo "batch-size" $p $size $threads >> $output_dir/$algo.txt
         done
     done
 done
@@ -113,9 +102,7 @@ cache_sizes=("92000")
 for p in 1 2 4 8 16 32 64 128; do
     for size in "${cache_sizes[@]}"; do
         for threads in "${threads_list[@]}"; do
-            for iteration in $(seq 1 $num_iteration); do
-                run_simulation $algo "k" $p $size $threads >> $output_dir/$algo/result_${iteration}.txt
-            done
+            run_simulation $algo "k" $p $size $threads >> $output_dir/$algo.txt
         done
     done
 done
