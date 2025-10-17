dd19e0ff523d3270ff3d33d8535e45e337ef79de
Lazy Donkey <lazy@donkey>
2025-10-17 20:34:16 +0700

    make it parallel

--- scripts/trace_stat.sh
+++ scripts/trace_stat.sh
@@ -4,10 +4,9 @@ BIN=~/libCacheSim/_build/bin/traceAnalyzer
 TRACE_LIST=~/Lazy-Promotions/scripts/current.txt
 TRACE_PATH=/ltdata/data/oracleReuse
 OUTPUT_DIR=~/trace_stats
+THREAD=128
 
-mkdir $OUTPUT_DIR
-cd $OUTPUT_DIR
+mkdir -p "$OUTPUT_DIR"
+cd "$OUTPUT_DIR"
 
-while IFS= read -r PATH; do
-    $BIN $TRACE_PATH/$PATH oracleGeneral --size
-done < $TRACE_LIST
+parallel -j $THREAD "$BIN" "$TRACE_PATH"/{} oracleGeneral --size :::: "$TRACE_LIST"
