7a36088f7917f37ad2980451c37fdf924786ee6f
Lazy Donkey <lazy@donkey>
2025-10-19 19:25:20 +0700

    update trace stats.sh

--- scripts/trace_stat.sh
+++ scripts/trace_stat.sh
@@ -1,12 +1,19 @@
 set -eux
 
-BIN=~/libCacheSim/_build/bin/traceAnalyzer
-TRACE_LIST=~/Lazy-Promotions/scripts/current.txt
+BIN=/users/muhhae/libCacheSim/_build/bin/traceAnalyzer
+TRACE_LIST=./current.txt
 TRACE_PATH=/ltdata/data/oracleReuse
-OUTPUT_DIR=~/trace_stats
-THREAD=32
+OUTPUT_DIR=/users/muhhae/trace_stats
 
-mkdir -p "$OUTPUT_DIR"
-cd "$OUTPUT_DIR"
+echo > ~/task
 
-parallel -j $THREAD "$BIN" "$TRACE_PATH"/{} oracleGeneral --size :::: "$TRACE_LIST"
+while IFS= read -r PATH; do
+    FILE=$TRACE_PATH/$PATH
+
+    size=$(stat --format="%s" "$FILE")
+    gb=$(( (size + 1024*1024*1024 - 1) / (1024*1024*1024) ))
+    min_dram=$(( gb/4+1 ))
+    priority=$(( 100/gb + 1 ))
+
+    echo "shell:$priority:$min_dram:1: mkdir -p $OUTPUT_DIR && cd $OUTPUT_DIR && $BIN $FILE oracleGeneral --size" >> ~/task
+done < $TRACE_LIST
